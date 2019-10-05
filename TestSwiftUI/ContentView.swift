9ca800c91f929d2494c544bdf302fec5ee90c37d
Ajisaputrars <Ajisaputrars@hotmail.com>
2019-10-06 02:25:22 +0700

    Initial Commit

---
+++ TestSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSwiftUI
+//
+//  Created by Aji Saputra Raka Siwi on 06/10/19.
+//  Copyright © 2019 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
